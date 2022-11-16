ea4d9a237d9bd2f067148907fa8de22e8e9c2670
yossefkhader <yossefkhader@campus.technion.ac.il>
2022-11-16 19:58:05 +0200

    decleration

--- RLEList.c
+++ RLEList.c
@@ -1,7 +1,11 @@
 #include "RLEList.h"
 
 struct RLEList_t{
-    //TODO: implement
+    char c;
+    int numOfOccurances;
+    RLEList next;
 };
 
+//yossef: 8,7,2,1
+//aaseel: 5,4,3,6
 //implement the functions here
